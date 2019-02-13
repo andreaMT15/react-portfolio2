b6052d7ce797fec432a61d58e1bb20eab98fce0d
Andrea Minhas <andrea.minhas@gmail.com>
2019-02-12 21:35:08 -0600

    re-created files

---
+++ client/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+app.post("/api/form", (req, res) => {
+  console.log(req.body);
+});
