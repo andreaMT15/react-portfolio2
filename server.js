dd634bc9d4c03e657c49fac78a4715b617dc4cbe
Andrea Minhas <andrea.minhas@gmail.com>
2019-02-12 21:39:21 -0600

    changed back end port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
