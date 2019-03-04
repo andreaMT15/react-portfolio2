99b0e7a4f9ecb917e4a23459343c82964c7195be
Andrea Minhas <andrea.minhas@gmail.com>
2019-03-03 23:58:23 -0600

    testing heroku again

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Thanks from "./pages/ThankYou";
 import Home from "../src/pages/Home";
 import MainNav from "./components/MainNav";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
+import Portfolio from "../src/pages/Portfolio";
+import Contact from "../src/pages/Contact";
 import "./Portfolio.css";
 
 class App extends Component {
