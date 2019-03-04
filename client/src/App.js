f161e5be49c2e6110721afdbba0dc3fb12562bee
Andrea Minhas <andrea.minhas@gmail.com>
2019-03-03 23:31:25 -0600

    testing heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Thanks from "./pages/ThankYou";
 import Home from "../src/pages/Home";
@@ -7,16 +7,20 @@ import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import "./Portfolio.css";
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <MainNav />
-      <Route path="/" exact component={Home} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/thank-you" component={Thanks} />
-    </div>
-  </Router>
-);
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <MainNav />
+          <Route path="/" exact component={Home} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/thank-you" component={Thanks} />
+        </div>
+      </Router>
+    );
+  }
+}
 
-export default AppRouter;
+export default App;
