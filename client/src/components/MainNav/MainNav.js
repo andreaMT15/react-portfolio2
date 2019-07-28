9a7e2d0999ded1a4b7205f720c6e3e9f9a01ce5a
Andrea Minhas <andrea.minhas@gmail.com>
2019-07-28 15:08:56 -0500

    removed commented out code

--- client/src/components/MainNav/MainNav.js
+++ client/src/components/MainNav/MainNav.js
@@ -5,21 +5,6 @@ import './MainNav.css';
 const MainNav = () => {
   return (
     <div className="nav">
-      {/* <Navbar fixed="top" className="nav" collapseOnSelect expand="lg">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto" />
-          <Nav>
-            <Nav.Link style={{ color: 'white' }} href="/">
-              Home
-            </Nav.Link>
-
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar> */}
       <ul className="nav-links">
         <Link to="/">
           <li>Home</li>
